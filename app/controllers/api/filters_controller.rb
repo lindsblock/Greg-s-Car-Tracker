0439b0ad7c19fbd3f6a6f2972471f827ba832243
Lindsay Block <lindsay.block05@gmail.com>
2018-08-14 10:36:41 -0600

    filters controller

--- app/controllers/api/filters_controller.rb
+++ app/controllers/api/filters_controller.rb
@@ -1,16 +1,42 @@
 class Api::FiltersController < ApplicationController
+  before_action :set_filter, only: [:update, :destroy, :show]
+
   def index
+    render json: Filter.all.order(created_at: :asc)
   end
 
   def show
+    render json: @filter
   end
 
   def create
+    filter = Filter.create(filter_params)
+    if filter.save
+      render json: filter
+    else
+      render json: {  errors: filter.errors.full_messages.join(','), status: 422 }
+    end
   end
 
   def update
+    if @filter.update(filter_params)
+      render json: @filter
+    else
+      render json: {  errors: filter.errors.full_messages.join(','), status: 422 }
+    end
   end
 
   def destroy
+    @filter.destroy
+  end
+
+  private
+
+  def filter_params
+    params.require(:filter).permit(:date, :complete, :oci, :miles)
+  end
+
+  def set_filter
+    @filter = Filter.find(params[:id])
   end
 end
--- client/src/components/Maintenance.js
+++ client/src/components/Maintenance.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Segment, Button, Form, Divider, Container } from 'semantic-ui-react';
+import { Table, Segment, Button, Form, Divider, Container, Header } from 'semantic-ui-react';
 import axios from 'axios';
 
 class Maintenance extends React.Component {
@@ -13,11 +13,19 @@ class Maintenance extends React.Component {
       oci: '',
       uoa: '',
     }],
-    showForm: false,
+    filters: [{
+      date: '',
+      complete: '',
+      oci:'',
+      miles: '',
+    }],
+    showServiceForm: false,
+    showFilterForm: false,
   }
 
   componentDidMount = () => {
     axios.get('api/services')
+    // axios.get('api/filters')
     .then( res => this.setState({ services: res.data }) )
   }
 
@@ -26,7 +34,7 @@ class Maintenance extends React.Component {
     this.setState({ [name]: value })
   }
 
-  handleSubmit = (e) => {
+  handleSubmitService = (e) => {
     e.preventDefault()
     axios.post('api/services', {...this.state })
     .then( () => {
@@ -34,45 +42,99 @@ class Maintenance extends React.Component {
     .then( res => this.setState({ services: res.data, showForm: false, work: '', date: '', price: '', miles: '', notes: '', oci: '', uoa: ''}) )
     })
   }
+  handleSubmitFilter = (e) => {
+    e.preventDefault()
+    axios.post('api/filters', {...this.state })
+    .then( () => {
+      axios.get('api/filters')
+    .then( res => this.setState({ services: res.data, showForm: false, date: '', complete: '', miles: '', oci: ''}) )
+    })
+  }
 
-  toggleForm = () => {
+  deleteService = (id) => {
+    axios.delete(`/api/services/${id}`)
+    let service = { ...this.state }
+    this.setState({ service })
+  }
+
+
+  toggleServiceForm = () => {
     this.setState( state => {
-      return { showForm: !state.showForm }
+      return { showServiceForm: !state.showServiceForm }
+    });
+  }
+
+  toggleFilterForm = () => {
+    this.setState( state => {
+      return { showFilterForm: !state.showFilterForm }
     });
   }
 
   render() {
-    const { services, showForm } = this.state
+    const { services, showServiceForm, showFilterForm, work, date, price, miles, notes, oci, uoa, filters } = this.state
     return(
       <div>
-          <Button color='black' style={styles.button} onClick={this.toggleForm}>{ showForm ? "Cancel" : "Add A Service" }</Button>
-            {/* { showForm ?
-              <Segment style={{ margin: '50px'}}>
-                <Form onSubmit={this.handleSubmit}>
+        <Header as="h1" style={styles.headers}>Maintenance</Header>
+        <Container textAlign="center">
+          <Button color="red" style={styles.button} onClick={this.toggleServiceForm}>{ showServiceForm ? "Cancel" : "Add A Service" }</Button>
+          <Divider hidden />
+            { showServiceForm ?
+              <Form onSubmit={this.handleSubmitService} style={{fontFamily: 'Ubuntu', fontSize:'18px'}}>
+                <Form.Group widths="equal">
                   <Form.Input
                     name="work"
-                    required
                     value={work}
                     onChange={this.handleChange}
                     label="Work Done"
 
                   />
-                  <Form.TextArea
+                  <Form.Input
                     name="date"
-                    required
                     value={date}
                     onChange={this.handleChange}
                     label="Date"
                     />
-                  <Button style={styles.button}>Submit</Button>
-                </Form>
-              </Segment> */}
-
+                  <Form.Input
+                    name="price"
+                    value={price}
+                    onChange={this.handleChange}
+                    label="Price"
+                    />
+                  <Form.Input
+                    name="miles"
+                    value={miles}
+                    onChange={this.handleChange}
+                    label="Miles"
+                    />
+                </Form.Group>
+                <Form.TextArea
+                  name="notes"
+                  value={notes}
+                  onChange={this.handleChange}
+                  label="notes"
+                />
+                <Form.Group widths="equal">
+                  <Form.Input
+                    name="oci"
+                    value={oci}
+                    onChange={this.handleChange}
+                    label="OCI"
+                    />
+                  <Form.Input
+                    name="uoa"
+                    value={uoa}
+                    onChange={this.handleChange}
+                    label="UOA"
+                    />
+                </Form.Group>
+                <Button  color="red" style={styles.button}>Submit</Button>
+              </Form>
+              :
             <div>
               <Table celled style={{fontFamily: 'Ubuntu', fontSize:'16px'}}>
                 <Table.Header>
                   <Table.Row>
-                    <Table.HeaderCell>Work</Table.HeaderCell>
+                    <Table.HeaderCell>Work Done</Table.HeaderCell>
                     <Table.HeaderCell>Install Date</Table.HeaderCell>
                     <Table.HeaderCell>Price</Table.HeaderCell>
                     <Table.HeaderCell>Miles</Table.HeaderCell>
@@ -93,15 +155,43 @@ class Maintenance extends React.Component {
                      <Table.Cell key={s.id}>{s.oci}</Table.Cell>
                      <Table.Cell key={s.id}>{s.uoa}</Table.Cell>
                      <Table.Cell>
-                       <Button circular icon="edit" size="small" onClick={this.editMod}/>
-                       <Button circular icon="delete" color="red" size="small" onClick={this.deleteMod}/>
+                       <Button circular icon="edit" size="small" onClick={this.editService}/>
+                       <Button circular icon="delete" color="red" size="small" onClick={this.deleteService}/>
                      </Table.Cell>
                    </Table.Row>
                  </Table.Body>
                )}
-              </Table>
-             </div>
-
+             </Table>
+             <Header as="h1" style={styles.headers}>Filters</Header>
+             <Button color="red" style={styles.button} onClick={this.toggleForm}>{ showFilterForm ? "Cancel" : "Add A Filter" }</Button>
+             <Table celled style={{fontFamily: 'Ubuntu', fontSize:'16px'}}>
+               <Table.Header>
+                 <Table.Row>
+                   <Table.HeaderCell>Date</Table.HeaderCell>
+                   <Table.HeaderCell>Filter Change?</Table.HeaderCell>
+                   <Table.HeaderCell>Miles on Filter</Table.HeaderCell>
+                   <Table.HeaderCell>OCI</Table.HeaderCell>
+                   <Table.HeaderCell>Edit/Delete</Table.HeaderCell>
+                 </Table.Row>
+               </Table.Header>
+               { filters.map( (f) =>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell key={f.id}>{f.date}</Table.Cell>
+                    <Table.Cell key={f.id}>{f.complete}</Table.Cell>
+                    <Table.Cell key={f.id}>$ {f.miles}</Table.Cell>
+                    <Table.Cell key={f.id}>{f.oci}</Table.Cell>
+                    <Table.Cell>
+                      <Button circular icon="edit" size="small" onClick={this.editService}/>
+                      <Button circular icon="delete" color="red" size="small" onClick={this.deleteService}/>
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              )}
+            </Table>
+           </div>
+         }
+        </Container>
       </div>
     )
   }
